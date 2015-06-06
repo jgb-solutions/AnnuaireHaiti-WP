0f37bad02f82e82d920347f240da0a63f973c05d
Jean Gerard <jgbneatdesign@gmail.com>
2015-06-06 16:17:53 -0400

    Loading the JS and CSS  from CDN directly

--- scripts.php
+++ scripts.php
@@ -2,10 +2,10 @@
 
 function ah_scripts()
 {
-    $url = 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js';
-    $f = fsockopen( $url, 80, $errno, $errstr, 30);
+    $url = 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js';
+    // $test_url = fopen( $url,'r' );
 
-    if ( $f !== false ) {
+    // if ( $test_url !== false ) {
         wp_deregister_script( 'jquery' );
         wp_register_script( 'jquery', $url );
 
@@ -13,11 +13,11 @@ function ah_scripts()
         wp_enqueue_style( 'ah-fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
         wp_enqueue_script( 'ah-bootrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js', array('jquery'), '', true );
 
-    } else {
-        wp_enqueue_style( 'ah-bootrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
-        wp_enqueue_style( 'ah-fontawesome', get_template_directory_uri() . '/css/font-awesome.min.css' );
-        wp_enqueue_script( 'ah-bootrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '', true );
-    }
+    // } else {
+    //     wp_enqueue_style( 'ah-bootrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
+    //     wp_enqueue_style( 'ah-fontawesome', get_template_directory_uri() . '/css/font-awesome.min.css' );
+    //     wp_enqueue_script( 'ah-bootrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '', true );
+    // }
 
     wp_enqueue_style( 'ah-gfonts', 'http://fonts.googleapis.com/css?family=Open+Sans:400italic,700,400' );
     wp_enqueue_style( 'ah-site', get_template_directory_uri() . '/css/site.css' );
