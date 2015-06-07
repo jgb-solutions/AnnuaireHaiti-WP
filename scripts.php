795a0f661a5167470496607afc9cc4bd92d8079a
Jean Gerard <jgbneatdesign@gmail.com>
2015-06-06 21:15:26 -0400

    Fixed some dependencies

--- scripts.php
+++ scripts.php
@@ -20,7 +20,7 @@ function ah_scripts()
     // }
     wp_enqueue_style( 'ah-gfonts', 'http://fonts.googleapis.com/css?family=Open+Sans:400italic,700,400' );
     wp_enqueue_style( 'ah-site', get_template_directory_uri() . '/css/site.css', array('ah-bootrap') );
-    wp_enqueue_script( 'ah-site-js', get_template_directory_uri() . '/js/site.js', array('jquery'), '', true );
+    wp_enqueue_script( 'ah-site-js', get_template_directory_uri() . '/js/site.js', array('jquery', 'ah-bootrap-js'), '', true );
     wp_localize_script(
         'ah-site-js',
         'AH_JS',
